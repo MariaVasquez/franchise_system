e1e890e83bfe22298f7af011d53498d5ee85998b
Maria Vasquez <mfvasquez17@outlook.com>
2025-06-27 05:33:28 -0500

    Se agrega server de produccion al swagger

--- src/main/java/com/mariavasquez/franchis/system/config/SwaggerConfig.java
+++ src/main/java/com/mariavasquez/franchis/system/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.mariavasquez.franchis.system.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +12,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://api.mvdeccenter.tech");
+        server.setDescription("Producción");
         return new OpenAPI()
                 .info(new Info()
                         .title("Franchise System API")
                         .version("1.0.0")
                         .description("API para gestión de franquicias y productos")
                 );
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/**")
+                .build();
     }
 }
